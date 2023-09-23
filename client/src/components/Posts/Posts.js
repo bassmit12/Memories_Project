a4f6c62b0ca5a1bab6daf7f087ae7f9630621f1c
Bas Smit <bas.smit@live.nl>
2023-09-23 17:55:28 +0200

    minor changes

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+const Posts = () => {
+  const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+
+  console.log(posts);
+  return (
+    <>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
